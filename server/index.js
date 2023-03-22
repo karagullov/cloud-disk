1e4f6bf5918794546a86da5f7c291dec284758dd
erlannn777 <erlank123456789@gmail.com>
2023-03-22 18:49:06 +0600

    [server] /auth/registration route added

--- server/index.js
+++ server/index.js
@@ -1,1 +1,23 @@
-console.log("Hello");
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+const authRouter = require("./routes/auth.routes");
+
+const app = express();
+const PORT = config.get("serverPort");
+
+app.use(express.json());
+app.use("/api/auth", authRouter);
+
+const start = async () => {
+  try {
+    await mongoose.connect(config.get("dbURL"));
+    app.listen(PORT, () => {
+      console.log("Server running on PORT: ", PORT);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
