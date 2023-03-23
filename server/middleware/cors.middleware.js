9636a6ade6c48c7f968524dbfb12ab18a027b2b2
erlannn777 <erlank123456789@gmail.com>
2023-03-23 11:01:40 +0600

    jwt auth added

--- server/middleware/cors.middleware.js
+++ server/middleware/cors.middleware.js
@@ -1,7 +1,7 @@
 function cors(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-  res.header("Access-Control-Allow-HEADERS", "Content-Type");
+  res.header("Access-Control-Allow-HEADERS", "Content-Type, Authorization");
   next();
 }
 
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
 const { check, validationResult } = require("express-validator");
-
+const authMiddleware = require("../middleware/auth.middleware");
 const router = new Router();
 
 router.post(
@@ -49,11 +49,36 @@ router.post("/login", async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
     const isPassValid = bcrypt.compareSync(password, user.password);
-
+    console.log(isPassValid);
     if (!isPassValid) {
       return res.status(404).json({ message: "Invalid password" });
     }
 
+    const token = jwt.sign({ id: user.id }, config.get("secretKey"), {
+      expiresIn: "1h",
+    });
+
+    return res.json({
+      token,
+      user: {
+        id: user.id,
+        email: user.email,
+        diskSpace: user.diskSpace,
+        usedSpace: user.usedSpace,
+        avatar: user.avatar,
+      },
+    });
+  } catch (e) {
+    console.log(e);
+    res.send({ message: "Server error" });
+  }
+});
+
+router.get("/auth", authMiddleware, async (req, res) => {
+  try {
+    console.log(req.user);
+    const user = await User.findOne({ _id: req.user.id });
+    console.log(user);
     const token = jwt.sign({ id: user.id }, config.get("secretKey"), {
       expiresIn: "1h",
     });
