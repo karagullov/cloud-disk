aa33e7cccd957479608243a2c70f4558c4121ae7
erlannn777 <erlank123456789@gmail.com>
2023-03-26 15:54:57 +0600

    sort added

--- server/controllers/file.controller.js
+++ server/controllers/file.controller.js
@@ -31,10 +31,29 @@ class FileController {
 
   async getFiles(req, res) {
     try {
-      const files = await File.find({
-        user: req.user.id,
-        parent: req.query.parent,
-      });
+      const { sort, parent } = req.query;
+      let files;
+
+      switch (sort) {
+        case "name":
+          files = await File.find({ user: req.user.id, parent }).sort({
+            name: 1,
+          });
+          break;
+        case "type":
+          files = await File.find({ user: req.user.id, parent }).sort({
+            type: 1,
+          });
+          break;
+        case "date":
+          files = await File.find({ user: req.user.id, parent }).sort({
+            date: 1,
+          });
+          break;
+        default:
+          files = await File.find({ user: req.user.id, parent });
+      }
+
       res.json(files);
     } catch (e) {
       console.log(e);
