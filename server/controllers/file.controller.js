e8c1a0fb4abae7070367d9753b7453c43f3625f6
erlannn777 <erlank123456789@gmail.com>
2023-03-26 17:40:59 +0600

    files view type plate added

--- server/controllers/file.controller.js
+++ server/controllers/file.controller.js
@@ -110,7 +110,7 @@ class FileController {
   async downloadFile(req, res) {
     try {
       const file = await File.findOne({ _id: req.query.id, user: req.user.id });
-      const path = `${filePath}${sep}${sep}${req.user.id}${sep}${sep}${file.path}`;
+      const path = fileService.getPath(file);
       if (fs.existsSync(path)) {
         return res.download(path, file.name);
       }
